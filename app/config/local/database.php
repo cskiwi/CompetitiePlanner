fbfb45877872c5477eca61d8cc554c867c277d97
Glenn.latomme <glenn.latomme@gmail.com>
2014-06-13 14:13:55 +0200

    db

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -51,7 +51,7 @@ return array(
                       'host' => 'localhost',
                       'database' => 'CompetitiePlanner',
                       'username' => 'root',
-                      'password' => '',
+                      'password' => 'Azerty123',
                       'charset' => 'utf8',
                       'collation' => 'utf8_unicode_ci',
                       'prefix' => '', ),
