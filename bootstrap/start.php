fc0dc97268ca3bf12fc1b5c6e61878b0f57088cd
Glenn Latomme <glenn.latomme@gmail.com>
2014-06-13 14:19:07 +0200

    finally

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -25,7 +25,7 @@ $app = new Illuminate\Foundation\Application;
 */
 
 $env = $app->detectEnvironment( array( 'local'      => array( 'GlennLompeLaptop' ),
-                                       'production' => array( 'comp-planner.be' ) ) );
+                                       'production' => array( 'Glenn-Vasten' ) ) );
 
 /*
 |--------------------------------------------------------------------------
